e02acdfd54e5e460c8bbe2ff23604432ecbf3d32
Nam Vo <hainam2412123.hnvh@gmail.com>
2025-04-05 11:32:16 +0700

    [f] Move from Github page to Netlify

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 This app runs directly in your browser and does not send any tracking data
 
-Home: https://tools.namvo.net/
+Home: https://img-tools.namvo.net/
--- vite.config.ts
+++ vite.config.ts
@@ -1,30 +1,14 @@
-import path, { resolve } from "path";
+import path from "path";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { copyFileSync } from "fs";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-    {
-      name: "copy-cname",
-      closeBundle() {
-        copyFileSync("CNAME", "build/CNAME");
-      },
-    },
-  ],
+  plugins: [react(), tailwindcss()],
   base: "",
   build: {
     outDir: "build",
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, "index.html"),
-        404: resolve(__dirname, "public/404.html"),
-      },
-    },
   },
   resolve: {
     alias: {
