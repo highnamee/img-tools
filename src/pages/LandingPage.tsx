ec7487a003f00defcaeb43d8125c60cdda357d20
Nam Vo <hainam2412123.hnvh@gmail.com>
2025-04-05 09:02:13 +0700

    [f] Update landing page

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -7,24 +7,40 @@ export default function LandingPage() {
     <div className="flex flex-col items-center py-12">
       <header className="mb-12 text-center">
         <h1 className="mb-16 font-extrabold">
-          <div className="mb-4 text-4xl">Welcome to</div>
-          <div className="from-primary to-primary/70 bg-gradient-to-r bg-clip-text text-7xl text-transparent">
+          <div className="mb-4 text-3xl sm:text-4xl">Say Hello to</div>
+          <div className="from-primary to-primary/70 bg-gradient-to-r bg-clip-text text-5xl text-transparent sm:text-7xl">
             IMG TOOLS
           </div>
         </h1>
-        <h2 className="mx-auto mb-8 text-xl">
-          <div>Experience seamless image processing directly in your browser.</div>
-          <div>Your data remains private and secure.</div>
+        <h2 className="mx-auto mb-8 text-lg sm:text-xl">
+          <div>Transform your images effortlessly, all within your browser.</div>
+          <div>No uploads, no tracking—just pure, private image editing.</div>
         </h2>
-        <div className="flex justify-center space-x-4">
-          <Button variant="default" size="lg" className="shadow-lg">
-            <Link to={CONVERTER_PATH}>Converter</Link>
-          </Button>
-          <Button variant="outline" size="lg" className="shadow-lg">
-            <Link to={METADATA_REMOVER_PATH}>Metadata Remover</Link>
+      </header>
+      <div className="grid grid-cols-1 gap-10 md:grid-cols-2">
+        <div className="rounded-lg bg-white p-6 shadow-lg">
+          <h3 className="mb-4 text-2xl font-bold">Image Converter</h3>
+          <ul className="list-disc space-y-2 pl-5">
+            <li>Seamlessly convert between formats: PNG, JPEG, WEBP, AVIF, BMP, TIFF</li>
+            <li>Compress and resize your images with ease</li>
+            <li>Batch process multiple files without any limits</li>
+          </ul>
+          <Button variant="default" size="lg" className="mt-6 shadow-lg">
+            <Link to={CONVERTER_PATH}>Give the Converter a Try</Link>
           </Button>
         </div>
-      </header>
+        <div className="rounded-lg bg-white p-6 shadow-lg">
+          <h3 className="mb-4 text-2xl font-bold">Metadata Remover</h3>
+          <ul className="list-disc space-y-2 pl-5">
+            <li>Unveil image metadata including GPS, Camera, Date taken, ...</li>
+            <li>Erase metadata instantly</li>
+            <li>Check file location on Google Maps, if available</li>
+          </ul>
+          <Button variant="outline" size="lg" className="mt-6 shadow-lg">
+            <Link to={METADATA_REMOVER_PATH}>Try Metadata Remover Now</Link>
+          </Button>
+        </div>
+      </div>
     </div>
   );
 }
